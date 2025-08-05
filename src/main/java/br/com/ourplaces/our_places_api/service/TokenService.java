32eb5e4e9360c8ac2e41e6f4e2259f0b0d3c3945
Iago Alves <iago.alves@groupsoftware.com.br>
2025-08-05 14:02:56 -0300

    feat: token service for authentication

---
+++ src/main/java/br/com/ourplaces/our_places_api/service/TokenService.java
@@ -0,0 +1,44 @@
+package br.com.ourplaces.our_places_api.service;
+
+import br.com.ourplaces.our_places_api.model.UserModel;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    private static final String ISSUER = "Our-Places-API";
+
+    public String generateToken(UserModel user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create().withIssuer(ISSUER).withSubject(user.getEmail()).withExpiresAt(generateExpirationDate()).sign(algorithm);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Error while generating JWT token", exception);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm).withIssuer(ISSUER).build().verify(token).getSubject();
+        } catch (JWTVerificationException exception) {
+            return "";
+        }
+    }
+
+    private Instant generateExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
